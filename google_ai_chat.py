d225424a9cf202cbcdc64ea075f8f0e384e714f8
tjhootman <tjhootman@gmail.com>
2025-05-25 14:26:18 -0400

    Add model, prompt and a streaming response

--- google_ai_chat.py
+++ google_ai_chat.py
@@ -4,9 +4,15 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv(), override=True)
 
-genai.configure(api_key=os.environ.get('GOOGLE_API_KEY'))
+genai.configure(api_key=os.environ.get('GEMINI_API_KEY'))
 
 # listing available models
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
+# for m in genai.list_models():
+#     if 'generateContent' in m.supported_generation_methods:
+#         print(m.name)
+
+model = genai.GenerativeModel('gemini-2.0-flash')
+prompt = 'Write an imaginary scientific paper that proposes a new theory of quantum mechanics.'
+response = model.generate_content(prompt, stream=True)
+for chunk in response:
+    print(chunk.text, end='')
